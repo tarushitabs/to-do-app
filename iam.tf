5057d7b2df0a91691f0ab002eaca32a46e7c58de
tarushibhandari <tarushi.bhandari@thoughtworks.com>
2022-04-28 18:50:53 +0530

    Tarushi| Add files

---
+++ iam.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "eks_g6-role" {
+  name = "eks_g6-cluster"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "eks_g6-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_g6-role.name
+}
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "eks_g6-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_g6-role.name
+}
+resource "aws_iam_role_policy_attachment" "eks_g6-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_g6-role.name
+}
+resource "aws_iam_role_policy_attachment" "eks_g6-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_g6-role.name
+}
+resource "aws_iam_role_policy_attachment" "eks_g6-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_g6-role.name
+}
